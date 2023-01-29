662122a1fa07d5cee3201acfb832f40aff5b036b
not-nom <nominated200@gmail.com>
2023-01-29 10:03:38 +0200

    Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {useRef, useState} from 'react'
 
 function App() {
+  const Ref = useRef('OK')
+  const [iV, iVC] = useState('')
+  const oCH = (e) => {
+    iVC(e.target.value)
+  }
+    
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> (not only) and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,8 +25,10 @@ function App() {
           Learn React
         </a>
       </header>
+      {iV === "Yes" ? null : <div className='modal-open'><input ref={Ref} onChange={oCH} value={iV} className='mod' placeholder='Do you like React?'/></div> }
     </div>
   );
 }
+
 
 export default App;
